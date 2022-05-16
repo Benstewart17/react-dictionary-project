de88d3bc44684e1bc9615cfc344def38cc3001c5
Benstewart17 <100318606+Benstewart17@users.noreply.github.com>
2022-05-16 18:38:14 -0400

    search update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
+import Dictionary from "./Dictionary";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Dictionary />
         <button className='btn btn-primary'>Hello World</button>
       </header>
     </div>
