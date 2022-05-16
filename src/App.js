d0d36b6af229b1c686da81b278508686e5bf4f0a
Benstewart17 <100318606+Benstewart17@users.noreply.github.com>
2022-05-16 15:47:15 -0400

    New-React-Dictionary-App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,14 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className='btn btn-primary'>Hello World</button>
       </header>
     </div>
   );
