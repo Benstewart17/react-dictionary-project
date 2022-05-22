be776c770acf6c4bb547b01cb457966ca98c3680
Benstewart17 <100318606+Benstewart17@users.noreply.github.com>
2022-05-22 14:27:03 -0400

    synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Synonyms from "./Synonyms";
+import "./Meaning.css"
 
 export default function Meaning(props) {
     console.log(props.meaning)
@@ -6,14 +8,18 @@ export default function Meaning(props) {
         <div className="Meaning">
         <h3>{props.meaning.partOfSpeech}</h3>
         {props.meaning.definitions.map(function(definition, index) {
+            console.log(definition)                      
             return (
                 <div key={index}>
                     <p>
+                        <strong>Definition: </strong>
                         {definition.definition}
                         <br />
-                        {definition.example}
+                        <strong>Example: </strong>
+                        <em>"{definition.example}"</em>
                     </p>
-                </div>
+                        <Synonyms synonyms={definition.synonyms} />
+                </div>  
             );
         })}  
         </div>
